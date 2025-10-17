07c6a82d05679078642bf173dd588e14c354667d
Arthur Madalena <arthurmadalena@gmail.com>
2025-10-16 21:21:42 -0300

    Criado a Controller

---
+++ src/main/java/dev/JavaTeste/cadastroDe/Controller.java
@@ -0,0 +1,16 @@
+package dev.JavaTeste.cadastroDe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagen nessa rota";
+    }
+}
